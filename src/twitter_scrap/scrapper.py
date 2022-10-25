71fbd75db5896349b8e4a10b00097af4aafe939d
AL HOUCEINE KILANI <alhouceine@lajavaness.com>
2022-10-25 06:54:27 +0200

    feat: Add username and user lacation if specified

--- src/twitter_scrap/scrapper.py
+++ src/twitter_scrap/scrapper.py
@@ -6,7 +6,7 @@ import tweepy
 from config import settings
 from utils import write_jsonlines
 
-limit = 100
+limit = 10000
 Path(settings.OUTPUTDIR).mkdir(exist_ok=True, parents=True)
 
 query = "#balancetonsalaire -is:retweet lang:fr"
@@ -16,7 +16,7 @@ tweets = tweepy.Paginator(
     query=query,
     tweet_fields=["context_annotations", "created_at", "geo", "author_id", "source"],
     max_results=100,
-).flatten(limit=10000)
+).flatten(limit=limit)
 
 data = []
 for tweet in tweets:
@@ -25,9 +25,23 @@ for tweet in tweets:
             "id": tweet.id,
             "text": tweet.text,
             "created_at": str(tweet.created_at),
-            "geo": tweet.geo,
             "author_id": tweet.author_id,
             "source": tweet.source,
         }
     )
+
+users = []
+start_index = 0
+chunks = 100
+for i in range(0, len(data), chunks):
+    users.extend(
+        client.get_users(
+            ids=[row["author_id"] for row in data[start_index : start_index + chunks]]
+        ).data
+    )
+    start_index += chunks
+
+data = [
+    {**row, "location": user.location, "username": user.username} for row, user in zip(data, users)
+]
 write_jsonlines(data=data, path=os.path.join(settings.OUTPUTDIR, "tweets.jsonlines"))
